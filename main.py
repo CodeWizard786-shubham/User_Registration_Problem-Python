b22e9be798145f80226c7f1ce3301f5572c537e4
shubham shirke <shirkeshubham84@gmail.com>
2023-06-18 13:52:42 +0530

    [shubham] Add: Added last name input for user.

--- main.py
+++ main.py
@@ -2,19 +2,29 @@
 @Author: shubham shirke
 @Date: 2023-06-18 11:30:30
 @Last Modified by: shubham shirke
-@Last Modified time: 2023-06-18 12:50:30
-@Title : Validate first name in user registration problem.
+@Last Modified time: 2023-06-18 13:40:30
+@Title : Validate last name in user registration problem.
 '''
 
-from log import logger
+from log import logger   
 import user_registration_problem as register
 
 # main
 def main():
+    """
+    Description : 
+            This function provide the user interface for the user on console and pass input as argument to required functions.
+    Parameters  :
+            none
+    Returns     :
+            none
+    """
     print("--User Registration Validation System--")
     first_name= input("Enter first name: ")
     register.validate_name(first_name)
-    
+    last_name= input("Enter last name: ")
+    register.validate_name(last_name)
+
 
 if __name__ == "__main__":
     main()
