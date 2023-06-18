bee39c2cccbff15aeb8f51eff8aa61160d15b383
shubham shirke <shirkeshubham84@gmail.com>
2023-06-18 20:05:11 +0530

    [shubham] Add: validate email function to match input email with regex pattern.

--- user_registration_problem.py
+++ user_registration_problem.py
@@ -20,3 +20,19 @@ def validate_name(name):
         logger.warning("Name not accepted: enter name with first letter as capital and minimum 3 characters.")
 
     
+def validate_email(email):
+    """
+    Description : 
+            This function validates email by matching user input string with regex pattern.
+    Parameters : 
+            email : user input email id
+    Returns    :
+            none
+    """
+    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    result = re.match(pattern, email) is not None
+    if result:
+        logger.info("Email id Accepted")
+    else:
+        logger.warning("Email not accepted: enter valid email id")
+
