68176e34ec6a0fbbda7c1d767b21b4d08506fb57
shubham shirke <shirkeshubham84@gmail.com>
2023-06-18 21:09:51 +0530

    [shubham] Add: Added password validation function to have minimum 8 characters.

--- user_registration_problem.py
+++ user_registration_problem.py
@@ -3,56 +3,76 @@ from log import logger
 
 import re
 
-def validate_name(name):
-    """
-    Description : 
-            This function validates first name by matching user input string with regex pattern.
-    Parameters : 
-            first_name : user input string
-    Returns    :
-            none 
-            prints message.
-    """
-    pattern = r"^[A-Z][a-z]{2,}$"
-    result = re.match(pattern, name) is not None
-    if result:
-        logger.info("Name Accepted")
-    else:
-        logger.warning("Name not accepted: enter name with first letter as capital and minimum 3 characters.")
+class UserRegistration:    
+
+        def validate_name(name):
+                """
+                Description : 
+                        This function validates first name by matching user input string with regex pattern.
+                Parameters : 
+                        first_name : user input string
+                Returns    :
+                        none 
+                        prints message.
+                """
+                pattern = r"^[A-Z][a-z]{2,}$"
+                result = re.match(pattern, name) is not None
+                if result:
+                        logger.info("Name Accepted")
+                else:
+                        logger.warning("Name not accepted: enter name with first letter as capital and minimum 3 characters.")
 
     
-def validate_email(email):
-    """
-    Description : 
-            This function validates email by matching user input string with regex pattern.
-    Parameters : 
-            email : user input email id
-    Returns    :
-            none
-            prints message.
-    """
-    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-    result = re.match(pattern, email) is not None
-    if result:
-        logger.info("Email id Accepted")
-    else:
-        logger.warning("Email not accepted: enter valid email id")
+        def validate_email(email):
+                """
+                Description : 
+                        This function validates email by matching user input string with regex pattern.
+                Parameters : 
+                        email : user input email id
+                Returns    :
+                        none
+                        prints message.
+                """
+                pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+                result = re.match(pattern, email) is not None
+                if result:
+                        logger.info("Email id Accepted")
+                else:
+                        logger.warning("Email not accepted: enter valid email id")
 
 
-def validate_phone_number(phone_number):
-    """
-    Description : 
-            This function validates phone number by matching user input string with regex pattern.
-            The regex pattern accepts phone number with +,2 digit country code and 10 digit phone number starting with 6-9.
-    Parameters : 
-            phone_number : user input phone number.
-    Returns    :
-            none
-            prints message.
-    """
-    pattern = r"^\+[0-9]{2}\s[6-9]\d{9}$"
-    result = re.match(pattern, phone_number)
-    if result:
-        logger.info("Phone Number Accepted")
-    else:
-        logger.warning("Phone Number not accepted: enter valid phone number with country code")
+        def validate_phone_number(phone_number):
+                """
+                Description : 
+                        This function validates phone number by matching user input string with regex pattern.
+                        The regex pattern accepts phone number with +,2 digit country code and 10 digit phone number starting with 6-9.
+                Parameters : 
+                        phone_number : user input phone number.
+                Returns    :
+                        none
+                        prints message.
+                """
+                pattern = r"^\+[0-9]{2}\s[6-9]\d{9}$"
+                result = re.match(pattern, phone_number)
+                if result:
+                        logger.info("Phone Number Accepted")
+                else:
+                        logger.warning("Phone Number not accepted: enter valid phone number with country code")
+
+        
+        def validate_password(password):
+                """
+                Description : 
+                        This function validates password to have minimum 8 characters.
+                Parameters : 
+                        password : user input string
+                Returns    :
+                        none
+                        prints message.
+                """
+                pattern = r"[A-Za-z\d]{8,}$"
+                result = re.match(pattern, password)
+                if result:
+                        logger.info("Password Accepted")
+                else:
+                        logger.warning("Password not accepted: enter valid password with minimum 8 characters")
