4f3c53526f84b57e6fcbc510f09a98e5131fc3de
shubham shirke <shirkeshubham84@gmail.com>
2023-06-18 21:08:01 +0530

    [shubham] Add: Input password for validation.

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 @Author: shubham shirke
 @Date: 2023-06-18 11:30:30
 @Last Modified by: shubham shirke
-@Last Modified time: 2023-06-18 16:15:30
-@Title : Validate phone number in user registration problem.
+@Last Modified time: 2023-06-18 17:40:30
+@Title : Validate password in user registration problem to have minimum 8 characters.
 '''
 
 from log import logger   
-import user_registration_problem as register
+from user_registration_problem import UserRegistration as register
 
 # main
 def main():
@@ -21,15 +21,26 @@ def main():
     """
     logger.info('Starting the program')
     print("--User Registration Validation System--")
+    
     first_name = input("Enter first name: ")
     register.validate_name(first_name)
+    
     last_name = input("Enter last name: ")
     register.validate_name(last_name)
+    
     email = input("Enter email id: ")
     register.validate_email(email)
+    
     phone_number = input("Enter Phone NUmber: ")
     register.validate_phone_number(phone_number)
+    
+    password = input("Enter Password: ")
+    register.validate_password(password)
+
+
+
     logger.info('Program finished')
+
 
 
 if __name__ == "__main__":
