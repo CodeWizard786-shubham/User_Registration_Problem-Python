2ed9052cacd879cf6333f52d95d20999695f4e92
shubham shirke <shirkeshubham84@gmail.com>
2023-06-18 20:36:00 +0530

    [shubham] Add: Input phone number for validation.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 @Author: shubham shirke
 @Date: 2023-06-18 11:30:30
 @Last Modified by: shubham shirke
-@Last Modified time: 2023-06-18 15:40:30
-@Title : Validate email in user registration problem.
+@Last Modified time: 2023-06-18 16:15:30
+@Title : Validate phone number in user registration problem.
 '''
 
 from log import logger   
@@ -19,6 +19,7 @@ def main():
     Returns     :
             none
     """
+    logger.info('Starting the program')
     print("--User Registration Validation System--")
     first_name = input("Enter first name: ")
     register.validate_name(first_name)
@@ -26,7 +27,9 @@ def main():
     register.validate_name(last_name)
     email = input("Enter email id: ")
     register.validate_email(email)
-
+    phone_number = input("Enter Phone NUmber: ")
+    register.validate_phone_number(phone_number)
+    logger.info('Program finished')
 
 
 if __name__ == "__main__":
