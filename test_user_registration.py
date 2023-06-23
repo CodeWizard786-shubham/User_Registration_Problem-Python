a3b531543f501a9f22a5c5aa9e6a1922b683122a
shubham shirke <shirkeshubham84@gmail.com>
2023-06-23 11:37:34 +0530

    [shubham] Refactor: Added more emails to validate.

--- test_user_registration.py
+++ test_user_registration.py
@@ -1,3 +1,4 @@
+
 '''
 @Author: shubham shirke
 @Date: 2023-06-19 11:30:30
@@ -13,26 +14,26 @@ class UserRegistrationTest(unittest.TestCase):
 
     # validate name for pass test
     def test_validate_name_valid(self):
-        name = "Test"
+        name = "Suresh"
         result = UserRegistration.validate_name(name)
         self.assertTrue(result)
 
     # validate name for fail test
     def test_validate_name_invalid(self):
-        name1 = "test"
+        name1 = "suresh"
         result = UserRegistration.validate_name(name1)
         self.assertFalse(result)
 
     # validate list of emails name for pass test
     def test_validate_email_valid(self):
-        emails = ["test12@gmail.com","abc@yahoo.com","abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com","abc+100@gmail.com"]
+        emails = ["abc12@gmail.com","abc@yahoo.com","abc-100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com","abc+100@gmail.com"]
         for email in emails:
             result = UserRegistration.validate_email(email)
             self.assertTrue(result)
 
     # validate list of emails for fail test
-    def test_validate_email_Invalid(self):
-        emails = ["test12@gmail","abc123@gmail.a","abc@gmail.com.1a","abc@abc@gmail.com","abc@%*.com","abc()*@gmail.com","abc123@.com"]
+    def test_validate_email_invalid(self):
+        emails = ["abc@.com.my","abc123@gmail.a","abc@gmail.com.1a","abc@abc@gmail.com","abc@%*.com","abc()*@gmail.com","abc123@.com","abc123@.com.com",".abc@abc.com","abc@gmail.com.aa.au","abc..2002@gmail.com"]
         for email in emails:
             result = UserRegistration.validate_email(email)
             self.assertFalse(result)
@@ -40,31 +41,31 @@ class UserRegistrationTest(unittest.TestCase):
     # validate phone number for pass test
     def test_validate_phone_number_valid(self):
         phone_number = "+91 9876543210"
-        result = UserRegistration.validate_email(phone_number)
+        result = UserRegistration.validate_phone_number(phone_number)
         self.assertTrue(result)
 
     # validate phone number for fail test
-    def test_validate_phone_number_valid(self):
+    def test_validate_phone_number_invalid(self):
         phone_number_1 = "+91 1876543210"
         phone_number_2 = "+9  9876543210"
-        result1 = UserRegistration.validate_email(phone_number_1)
+        result1 = UserRegistration.validate_phone_number(phone_number_1)
         self.assertFalse(result1)
-        result2 = UserRegistration.validate_email(phone_number_2)
+        result2 = UserRegistration.validate_phone_number(phone_number_2)
         self.assertFalse(result2)
 
     # validate password for pass test
     def test_validate_password_valid(self):
         password = "Testexample@123"
-        result = UserRegistration.validate_email(password)
-        self.assertFalse(result)
+        result = UserRegistration.validate_password(password)
+        self.assertTrue(result)
 
     # validate password for fail test
-    def test_validate_password_valid(self):
-        password_1 = "test@123"
-        password_2 = "test123"
-        result1 = UserRegistration.validate_email(password_1)
+    def test_validate_password_invalid(self):
+        password_1 = "abc@123"
+        password_2 = "abc123"
+        result1 = UserRegistration.validate_password(password_1)
         self.assertFalse(result1)
-        result2 = UserRegistration.validate_email(password_2)
+        result2 = UserRegistration.validate_password(password_2)
         self.assertFalse(result2)
         
 
